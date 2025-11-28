112c3195949fc7140c526dcff6e368dc4d2e7b5b
Herve Desrosiers <hdesrosiers@stingray.com>
2025-11-28 07:13:43 -0500

    otdb questions fetch implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await fetch("https://opentdb.com/api.php?amount=5");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setQuestions(data.results);
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchQuestions();
+  }, []);
+
+  if (loading) {
+    return <div>Loading questions...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  console.log(questions);
+
+  const questionElements = questions
+    ? questions.map((item, index) => {
+        return <h3 key={index}>{item.question}</h3>;
+      })
+    : null;
+  console.log(questionElements);
+
   return (
     <>
-      <p>Quizzical</p>
+      <h1>Quizzical</h1>
+      {questionElements}
     </>
   );
 }
